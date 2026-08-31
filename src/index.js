80d7716fe1d698fef43d5c029908eb7ef01dfb74
Leakey-Hans <226905899+Leakey-Hans@users.noreply.github.com>
2026-08-31 17:20:22 +0300

    syncify-to-do
    Sidebar complete

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import "./styles.css"
+
+const sideBtns = document.querySelectorAll(".list-item");
+
+sideBtns.forEach(item => {
+    item.addEventListener('click', () => {
+        //remove current active item
+        document.querySelector(".list-item.active")?.classList.remove('active');
+        //added to clicked item
+        item.classList.add('active');
+    });
+});
