9cb907980cdfdd609b629cfa1d09e9904b4ffa23
Leakey-Hans <226905899+Leakey-Hans@users.noreply.github.com>
2026-09-08 13:53:55 +0300

    syncify-to-do
    Transferred dom.js module contents to index.js and uiInteractions.JS

--- src/uiInteractions.js
+++ src/uiInteractions.js
@@ -1,3 +1,5 @@
+import { createProjectCard  } from "./index.js";
+
 //Sidebar list interactivity
 const sideBtns = document.querySelectorAll(".list-item");
 
@@ -8,4 +10,25 @@ sideBtns.forEach(item => {
         //added to clicked item
         item.classList.add('active');
     });
+});
+
+//Project Addition
+    //Global Vars
+const newProjectBtn = document.querySelector('.project-button');
+const addProjectModal = document.querySelector('.project-modal');
+const addProjectBtn = document.querySelector('.add-project');
+
+    //Event Listeners
+
+addProjectBtn.addEventListener('click', () => {
+    createProjectCard();
+    addProjectModal.classList.remove('project-modal-opened');
+    newProjectBtn.disabled = false;
+    const modalForm = document.querySelector('.modal-form');
+    modalForm.reset();
+});
+
+newProjectBtn.addEventListener("click", () => {
+    addProjectModal.classList.add('project-modal-opened');
+    newProjectBtn.disabled = true;
 });
